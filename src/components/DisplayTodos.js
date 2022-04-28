1d99facc3d8089d5dcaa4e7a85631eb1e336b659
Mario Torres <marioytorres@gmail.com>
2022-04-28 13:59:41 -0400

    feat: add functional logic and redux
    Todo, DisplayTodos and TodoItem functionalities and Redux state store.

--- src/components/DisplayTodos.js
+++ src/components/DisplayTodos.js
@@ -1,30 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import {
+  addTodos,
+  completeTodos,
+  removeTodos,
+  updateTodos,
+} from "../redux/reducer";
 import TodoItem from "./TodoItem";
 
-const DisplayTodos = () => {
-  const sampleItems = [
-    { id: 1, item: "Hola Mundo 1" },
-    { id: 2, item: "Hola Mundo 2" },
-    { id: 3, item: "Hola Mundo 3" },
-  ];
+const mapStateToProps = (state) => {
+  return {
+    todos: state,
+  };
+};
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (obj) => dispatch(addTodos(obj)),
+    removeTodo: (id) => dispatch(removeTodos(id)),
+    updateTodo: (obj) => dispatch(updateTodos(obj)),
+    completeTodo: (id) => dispatch(completeTodos(id)),
+  };
+};
+
+const DisplayTodos = (props) => {
+  const [sort, setSort] = useState("active");
   return (
     <div className="displaytodos">
       <div className="buttons">
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
+        <button onClick={() => setSort("all")}>All</button>
+        <button onClick={() => setSort("active")}>Active</button>
+        <button onClick={() => setSort("completed")}>Completed</button>
       </div>
 
       <ul>
-        {sampleItems.length > 0
-          ? sampleItems.map((item) => {
-              return <TodoItem key={item.id} item={item} />;
+        {props.todos.length > 0 && sort === "active"
+          ? props.todos.map((item) => {
+              return (
+                item.completed === false && (
+                  <TodoItem
+                    key={item.id}
+                    item={item}
+                    removeTodo={props.removeTodo}
+                    updateTodo={props.updateTodo}
+                    completeTodo={props.completeTodo}
+                  />
+                )
+              );
             })
-          : "NO TODO LIST"}
+          : null}
+        {/* for completed items */}
+        {props.todos.length > 0 && sort === "completed"
+          ? props.todos.map((item) => {
+              return (
+                item.completed === true && (
+                  <TodoItem
+                    key={item.id}
+                    item={item}
+                    removeTodo={props.removeTodo}
+                    updateTodo={props.updateTodo}
+                    completeTodo={props.completeTodo}
+                  />
+                )
+              );
+            })
+          : null}
+        {/* for all items */}
+        {props.todos.length > 0 && sort === "all"
+          ? props.todos.map((item) => {
+              return (
+                <TodoItem
+                  key={item.id}
+                  item={item}
+                  removeTodo={props.removeTodo}
+                  updateTodo={props.updateTodo}
+                  completeTodo={props.completeTodo}
+                />
+              );
+            })
+          : null}
       </ul>
     </div>
   );
 };
 
-export default DisplayTodos;
+export default connect(mapStateToProps, mapDispatchToProps)(DisplayTodos);
